c700d3d723700c1db00ff088e0bcc0224cd35329
Stuart Berg <bergs@janelia.hhmi.org>
2012-10-24 14:39:45 -0400

    clusterized executable no longer specifically references the PixelClassification workflow.

--- ilastik/workflow.py
+++ ilastik/workflow.py
@@ -1,8 +1,11 @@
-from abc import ABCMeta, abstractproperty
+from abc import abstractproperty
+from ilastik.utility.subclassRegistry import SubclassRegistryMeta
+
+class WorkflowMeta(SubclassRegistryMeta):
+    pass
 
 class Workflow( object ):
-    
-    __metaclass__ = ABCMeta # Force subclasses to override abstract methods and properties
+    __metaclass__ = WorkflowMeta # Provides Workflow.all_subclasses member
 
     @abstractproperty
     def applets(self):
@@ -15,3 +18,11 @@ class Workflow( object ):
     @property
     def finalOutputSlot(self):
         return None
+
+    @classmethod
+    def getSubclass(cls, name):
+        for subcls in cls.all_subclasses:
+            if subcls.__name__ == name:
+                return subcls
+        raise RuntimeError("No known workflow class has name " + name)
+
